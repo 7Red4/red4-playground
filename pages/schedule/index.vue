fc6a30d7d055cc1a5e0059d7ddaa4a22cfd41948
Red <abc401344@gmail.com>
2019-08-20 16:08:40 +0800

    test

--- pages/schedule/index.vue
+++ pages/schedule/index.vue
@@ -4,7 +4,7 @@
       <v-card>
         <v-card-text>
           <v-btn color="primary" tag="label">
-            上傳你的CSV
+            上傳你的 CSV
             <input type="file" hidden accept="csv" @input="previewCsv" />
           </v-btn>
           <v-textarea outline readonly v-model="result" height="600px"></v-textarea>
@@ -43,20 +43,23 @@ export default {
         var allTextLines = csv.replace(/\n超級/g, "超級").split(/\n/);
         var date = "";
         var results = "";
-          //日期,時間,節目表,集數,節目類別,級別
-        
+        //日期,時間,節目表,集數,節目類別,級別
+
         for (let i = 1; i < allTextLines.length; i++) {
           var data = allTextLines[i].split(",");
-          let 日期 = data[0]
-          let 時間 = data[1]
-          let 節目表 = data[2]
-          let 集數 = data[3]
-          let 節目類別 = data[4]
-          let 級別 = data[5]
+          let 日期 = data[0];
+          let 時間 = data[1];
+          let 節目表 = data[2];
+          let 集數 = data[3];
+          let 節目類別 = data[4];
+          let 級別 = data[5];
           var tarr = [];
-          if(date != "" && date == 日期){
+          if (date != "" && date == 日期) {
             results += `
-<ul><li>${時間}</li><li class="li_program">${節目表.replace(/"|\n/g,"")}</li><li>${集數}</li><li>${節目類別}</li><li>普</li></ul>`;
+<ul><li>${時間}</li><li class="li_program">${節目表.replace(
+              /"|\n/g,
+              ""
+            )}</li><li>${集數}</li><li>${節目類別}</li><li>普</li></ul>`;
           } else {
             results += `
 <strong>播出時間: ${日期} ()</strong>
